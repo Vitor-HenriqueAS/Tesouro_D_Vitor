01c434a7ba423a67c35ca15b91698c3afde502f6
Vitor-HenriqueAS <vitorhenrique.a12@gmail.com>
2023-09-10 11:23:22 -0300

    create component section and otimization code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,108 +2,84 @@
 
 import { useState } from "react";
 import Logo from "@/components/Logo";
-import Secoes from "@/components/Secoes";
-import PersonImg from "@/components/PersonImg";
+import Section from "@/components/Section";
 import styles from "./styles/page.module.css";
 
 export default function Home() {
-  const [selecionarTema, setselecionarTema] = useState(true);
-  const [selecionarTexto1, setSelecionarTexto1] = useState<string>("east-blue");
-  const [selecionarTexto2, setSelecionarTexto2] = useState<string>("alvida");
-  const [companheiroIndex, setCompanheiroIndex] = useState(0);
-  const [adversarioIndex, setAdversarioIndex] = useState(0);
-  const companheiros = ["east-blue","goldroger","luffy","koby","zoro","shanks","nami","usopp","sanji",];
+  const [state, setState] = useState({
+    selecionarTema: true,
+    selecionarTexto1: "east-blue",
+    selecionarTexto2: "alvida",
+    companheiroIndex: 0,
+    adversarioIndex: 0,
+  });
+
+  const { selecionarTema, selecionarTexto1, selecionarTexto2, companheiroIndex, adversarioIndex } = state;
+
+  const companheiros = ["east-blue", "goldroger", "luffy", "koby", "zoro", "shanks", "nami", "usopp", "sanji"];
   const adversarios = ["alvida", "morgan", "buggy", "garp", "kuro", "mihawk", "arlong"];
 
-  const alterarTema = () => setselecionarTema(!selecionarTema);
-  const alteraTexto1 = (personagem: string) => setSelecionarTexto1(personagem);
-  const alteraTexto2 = (personagem: string) => setSelecionarTexto2(personagem);
-  const alteraCompanheiro = (botao: boolean) => {
-    botao ?
-    (companheiroIndex < companheiros.length - 1) ? setCompanheiroIndex(companheiroIndex + 1) : null
-    :
-    (companheiroIndex > 0) ? setCompanheiroIndex(companheiroIndex - 1) : null
-    setSelecionarTexto1(companheiros[companheiroIndex])
+  const toggleTema = () => {
+    setState({ ...state, selecionarTema: !selecionarTema });
   };
 
-  const alteraAdversario = (botao: boolean) => {
-    botao ?
-    (adversarioIndex < adversarios.length - 1) ? setAdversarioIndex(adversarioIndex + 1) : null
-    :
-    (adversarioIndex > 0) ? setAdversarioIndex(adversarioIndex - 1) : null
-    setSelecionarTexto2(adversarios[adversarioIndex])
+  const alteraTexto = (personagem: string, isCompanheiro: boolean) => {
+    const index = isCompanheiro ? companheiroIndex : adversarioIndex;
+    const newText = isCompanheiro ? companheiros[index] : adversarios[index];
+    setState({ ...state, [isCompanheiro ? "selecionarTexto1" : "selecionarTexto2"]: newText });
+  };
+
+  const alteraPersonagem = (isCompanheiro: boolean, botao: boolean) => {
+    const index = isCompanheiro ? companheiroIndex : adversarioIndex;
+    const length = isCompanheiro ? companheiros.length : adversarios.length;
+    if (botao) {
+      if (index < length - 1) {
+        setState({ ...state, [isCompanheiro ? "companheiroIndex" : "adversarioIndex"]: index + 1 });
+      }
+    } else {
+      if (index > 0) {
+        setState({ ...state, [isCompanheiro ? "companheiroIndex" : "adversarioIndex"]: index - 1 });
+      }
+    }
   };
 
   return (
     <>
       <main>
-        <section
-          className={`${styles.module} ${styles.parallax} ${styles.parallax_1}`}
-        >
+        <section className={`${styles.module} ${styles.parallax} ${styles.parallax_1}`}>
           <div className={styles.cabecalho}>
             <Logo qualLogo={selecionarTema} />
             <h1 className={styles.title}>Uma aventura sem limites</h1>
           </div>
-          {/* BOTÕES */}
           <div className={styles.buttons}>
-            <button type="button" onClick={alterarTema}>
+            <button type="button" onClick={toggleTema}>
               {selecionarTema ? "IR PARA A SÉRIE" : "IR PARA O ANIME"}
             </button>
           </div>
         </section>
 
         <section className={`${styles.module} ${styles.content}`}>
-          <div className={styles.manga}>
-            <div className={styles.container}>
-              <Secoes qualTema={selecionarTema} qualTexto={selecionarTexto1} ilha="east-blue"/>
-              <div className={styles.personagens}>
-                {companheiros.map((personagem) => (
-                  <PersonImg
-                    key={personagem}
-                    qualImagens={selecionarTema}
-                    personagem={personagem}
-                    ClicouNoPersonagem={alteraTexto1}
-                  />
-                ))}
-              </div>
-
-              <div className={styles.container__button}>
-                <button type="button" onClick={() => alteraCompanheiro(false)}>Voltar</button>
-                <button type="button" onClick={() => alteraCompanheiro(true)}>Próximo</button>
-              </div>
-
-            </div>
-          </div>
+          <Section
+            qualTema={selecionarTema}
+            qualTexto={selecionarTexto1}
+            personagens={companheiros}
+            alteraTexto={(personagem: string) => alteraTexto(personagem, true)}
+            alteraPersonagem={(botao: boolean) => alteraPersonagem(true, botao)}
+          />
         </section>
 
-        <section
-          className={`${styles.module} ${styles.parallax} ${styles.parallax_2}`}
-        >
+        <section className={`${styles.module} ${styles.parallax} ${styles.parallax_2}`}>
           <h1></h1>
         </section>
 
         <section className={`${styles.module} ${styles.content}`}>
-          <div className={styles.manga}>
-            <div className={styles.container}>
-              <Secoes qualTema={selecionarTema} qualTexto={selecionarTexto2} ilha="east-blue"/>
-              <div className={styles.personagens}>
-                {adversarios.map((personagem) => (
-                  <PersonImg
-                    key={personagem}
-                    qualImagens={selecionarTema}
-                    personagem={personagem}
-                    ClicouNoPersonagem={alteraTexto2}
-                  />
-                ))}
-              </div>
-
-              <div className={styles.container__button}>
-                <button type="button" onClick={() => alteraAdversario(false)}>Voltar</button>
-                <button type="button" onClick={() => alteraAdversario(true)}>Próximo</button>
-              </div>
-
-            </div>
-          </div>
+          <Section
+            qualTema={selecionarTema}
+            qualTexto={selecionarTexto2}
+            personagens={adversarios}
+            alteraTexto={(personagem: string) => alteraTexto(personagem, false)}
+            alteraPersonagem={(botao: boolean) => alteraPersonagem(false, botao)}
+          />
         </section>
       </main>
     </>
---
+++ src/components/Section/index.tsx
@@ -0,0 +1,37 @@
+import Secoes from "@/components/Secoes";
+import PersonImg from "@/components/PersonImg";
+import styles from "./section.module.css";
+
+interface SectionProps {
+  qualTema: boolean
+  qualTexto: string
+  personagens: string[];
+  alteraTexto: (personagem: string) => void;
+  alteraPersonagem: (botao: boolean) => void;
+}
+
+export default function Section({ qualTema, qualTexto, personagens, alteraTexto, alteraPersonagem }: SectionProps) {
+  return (
+    <>
+      <div className={styles.manga}>
+        <div className={styles.container}>
+          <Secoes qualTema={qualTema} qualTexto={qualTexto} ilha="east-blue" />
+          <div className={styles.personagens}>
+            {personagens.map((personagem) => (
+              <PersonImg
+                key={personagem}
+                qualImagens={qualTema}
+                personagem={personagem}
+                ClicouNoPersonagem={alteraTexto}
+              />
+            ))}
+          </div>
+          <div className={styles.container__button}>
+            <button type="button" onClick={() => alteraPersonagem(false)}>Voltar</button>
+            <button type="button" onClick={() => alteraPersonagem(true)}>Próximo</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
